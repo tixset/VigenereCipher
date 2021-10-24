eeae92ec3c6ac553f2cd0f305d51649b50fa1bb9
tixset <tixset@mail.ru>
2021-10-24 23:54:20 +0300

    Update code.py

--- python/code.py
+++ python/code.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 # функция не работает, функция "chr" в python не может иметь отрецательное значение / the function does not work, the "chr" function in python cannot have a negative value
 def code(text, password, decode):
-        if len(password) == 0:
-                exit;
-        sign = 1
-        if decode:
-                sign = -1
-        result = ""
-        for i in range(0, len(text)):
-                result += chr(ord(text[i]) + sign * ord(password[i % len(password) + 1]))
-        return result
+    if len(password) == 0:
+        exit;
+    sign = 1
+    if decode:
+        sign = -1
+    result = ""
+    for i in range(0, len(text)):
+        result += chr(ord(text[i]) + sign * ord(password[i % len(password) + 1]))
+    return result
