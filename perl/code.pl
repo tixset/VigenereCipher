323d8ce442efb8875206b58db576f316a294e0f2
tixset <tixset@mail.ru>
2021-10-24 17:45:54 +0300

    Create code.pl

---
+++ perl/code.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use bytes;
+sub code{
+        my ($text, $password, $decode) = (shift, shift, shift);
+        exit 1 if length($password) == 0;
+        my $sign = 1;
+        $sign = -1 if $decode == 1;
+        my $result = "";
+        for (my $i=0; $i <= length($text) - 1; $i++) {
+                my $e1 = $sign * ord(substr($password, $i % length($password), 1));
+                $result = $result . chr(ord(substr($text, $i, 1)) + $e1);
+        }
+        return $result;
+}
