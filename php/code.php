46f5f2b5726db541b63a63d1f36b9f267c323134
tixset <tixset@mail.ru>
2021-10-24 06:15:39 +0300

    Update code.php

--- php/code.php
+++ php/code.php
@@ -1,10 +1,10 @@
 function code($text, $password, $decode) {
-    $passlen = iconv_strlen($password, 'UTF-8');
+    $passlen = strlen($password);
     if ($passlen == 0) exit;
     $sign = ($decode) ? -1 : 1;
     $result = '';
-    for ($i = 1; $i < iconv_strlen($text, 'UTF-8') + 1; $i++){
-        $result = $result.chr(ord(mb_substr($text, ($i - 1), 1, 'UTF-8')) + $sign * ord(mb_substr($password, (($i % $passlen + 1) - 1), 1, 'UTF-8')));
+    for ($i = 1; $i < strlen($text) + 1; $i++){
+        $result = $result.chr(ord(substr($text, ($i - 1), 1)) + $sign * ord(substr($password, (($i % $passlen + 1) - 1), 1)));
     }
-    return $result; 
+    return $result;
 }
