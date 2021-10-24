bf425b9736229a6e9fd543a623d3d56378908175
tixset <tixset@mail.ru>
2021-10-24 05:28:31 +0300

    Create code.js

---
+++ javascript/code.js
@@ -0,0 +1,10 @@
+function code(text, key, decode) {
+  var sign;
+    var result = "";
+    if (key.length == 0) return;
+    (decode) ? sign = -1 : sign = 1;
+    for (let i = 0; i < text.length; i++) {
+        result = result + String.fromCharCode(text[i].charCodeAt(0) + sign * key[i % key.length].charCodeAt(0));
+    }
+    return result;
+}
