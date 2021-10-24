5fec619c009a726933958266d6c1a020a4c8d4b4
tixset <tixset@mail.ru>
2021-10-24 05:25:37 +0300

    Create code.pas

---
+++ pascal\delphi/code.pas
@@ -0,0 +1,11 @@
+function Code(text: string; password: string; decode: boolean):string;
+var
+    i, sign: integer;
+begin
+    if length(password) = 0 then Exit;
+    if decode then sign := -1 else sign := 1;
+    for i := 1 to Length(text) do
+    begin
+      result :=result+ chr(ord(text[i]) + sign * ord(password[i mod length(password) + 1]));
+    end;
+end;
