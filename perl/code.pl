9484eb2fdce0d6770cefede2fdd55c34ec22f70b
tixset <tixset@mail.ru>
2021-10-24 23:56:00 +0300

    Update code.pl

--- perl/code.pl
+++ perl/code.pl
@@ -3,13 +3,13 @@ use strict;
 use warnings;
 use bytes;
 sub code {
-        my ($text, $password, $decode) = (shift, shift, shift);
-        exit 1 if length($password) == 0;
-        my $sign = 1;
-        $sign = -1 if $decode == 1;
-        my $result = "";
-        for (my $i = 0; $i <= length($text) - 1; $i++) {
-                $result .= chr(ord(substr($text, $i, 1)) + $sign * ord(substr($password, $i % length($password), 1)));
-        }
-        return $result;
+    my ($text, $password, $decode) = (shift, shift, shift);
+    exit 1 if length($password) == 0;
+    my $sign = 1;
+    $sign = -1 if $decode == 1;
+    my $result = "";
+    for (my $i = 0; $i <= length($text) - 1; $i++) {
+        $result .= chr(ord(substr($text, $i, 1)) + $sign * ord(substr($password, $i % length($password), 1)));
+    }
+    return $result;
 }
